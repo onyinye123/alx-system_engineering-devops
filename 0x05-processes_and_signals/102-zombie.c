c8b2b03abbc3ffc077c8fa59d80c7ba0d37bb62d
onyinye123 <onyinyeudoba@gmail.com>
2023-01-27 21:00:00 -0800

    signal

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,36 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#!/usr/bin/env bash
+# Manages the script manage_my_process.
+#   When passed the argument `start`:
+#     1. Starts manage_my_process
+#     2. Creates a file containings its PID in /var/run/my_process.pid
+#     3. Displays "manage_my_process started"
+#   When passed the argument `stop`:
+#     1. Stops manage_my_process
+#     2. Deletes the file /var/run/my_process.pid
+#     3. Displays "manage_my_process stopped"
+#   When passed the argument `restart`:
+#     1. Stops manage_my_process
+#     2. Deletes the file /var/run/my_process.pid
+#     3. Starts manage_my_process
+#     4. Creates a file containing its PID in /var/run/my_process.pid
+#     5. Displays "manage_my_process restarted"
+#   If any other or no arguments are passed, displays
+#+  "Usage: manage_my_process {start|stop|restart}"
 
-/**
- * infinite_while - creates an infinite loop to make the program hang
- * Return: always 0
- */
-int infinite_while(void)
-{
-	while (1)
-	{
-		sleep(1);
-	}
-	return (0);
-}
-
-/**
- * main - creates 5 zombie processes
- * Return: always 0
- */
-int main(void)
-{
-	int i;
-	pid_t zombie;
-
-	for (i = 0; i < 5; i++)
-	{
-		zombie = fork();
-		if (!zombie)
-			return (0);
-		printf("Zombie process created, PID: %d\n", zombie);
-	}
-
-	infinite_while();
-	return (0);
+if [ "${1}" == "start" ]
+then
+    ./manage_my_process &
+    touch /var/run/my_process.pid
+    echo "$!" > /var/run/my_process.pid
+    echo "manage_my_process started"
+elif [ "${1}" == "stop" ]
+then
+    echo "manage_my_process stopped"
+    kill "$(cat /var/run/my_process.pid)"
+    rm /var/run/my_process.pid
+elif [ "${1}" == "restart" ]
+then
+    kill "$(cat /var/run/my_process.pid)"
+    rm /var/run/my_process.pid
+    ./manage_my_process &
+    touch /var/run/my_process.pid
+    echo "$!" > /var/run/my_process.pid
+    echo "manage_my_process restarted"
+else
+    echo "Usage: manage_my_process {start|stop|restart}"
+fi
